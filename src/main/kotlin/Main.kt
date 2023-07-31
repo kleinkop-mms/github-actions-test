c5050478d2fae92c6609bcf8027f28a8f908dc25
Udo Kleinkop <kleinkop@mediamarktsaturn.com>
2023-07-31 18:30:34 +0200

    any change

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    println("Hello MMS world!")
+    println("Hello MMS!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
